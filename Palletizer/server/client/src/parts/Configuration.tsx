064ae0f09fd12ad5d42d94e6670e635d34087e9a
AT <vlex@Alexs-MacBook-Pro.local>
2020-06-11 05:54:04 -0400

    editor prototyping

--- Palletizer/server/client/src/parts/Configuration.tsx
+++ Palletizer/server/client/src/parts/Configuration.tsx
@@ -7,25 +7,30 @@ import {ConfigState} from "../types/Types";
 // Styles
 import "./css/Configuration.scss";
 import "./css/Login.scss";
+import "./css/Editor.scss";
+
+// Editor should be a popup.
 
 
 interface ConfigCellProps {
-    title: string;
+    file_name: string;
+    start_editor(fn:string) : any;
 }
 
-function ConfigCell({title} : ConfigCellProps) {
+function ConfigCell({file_name, start_editor} : ConfigCellProps) {
 
     let handle_edit = () => {
-        console.log("Edit config " + title);
+        console.log("Edit config " + file_name);
+        start_editor(file_name);
     };
 
     let handle_delete = () => {
-        console.log("Delete config " + title);
+        console.log("Delete config " + file_name);
     };
     
     return (
         <div className="ConfigCell">
-            <span> {title} </span>
+            <span> {file_name} </span>
             <div className="EditConfigButton" onClick={handle_edit}>
                 <span> Edit </span>
             </div>
@@ -44,16 +49,18 @@ function ConfigCell({title} : ConfigCellProps) {
 interface ConfigContainerProps {
     title: string;
     configs: string[];
+    start_editor(fn: string) : any;
 }
 
 
 function ConfigContainer(props: ConfigContainerProps) {
 
-    let {title, configs} = props;
+    let {title, configs, start_editor} = props;
 
     let handle_add = ()=>{
         console.log("Handle: Add " + title);
     };
+
     
     return(
         <div className="ConfigContainer">
@@ -61,8 +68,8 @@ function ConfigContainer(props: ConfigContainerProps) {
                 <span> {title} </span>
             </div>
             <div className="ConfigScroll" >
-                {configs.map((title, index)=>{
-                    return (<ConfigCell title={title} key={index} />)
+                {configs.map((file_name, index)=>{
+                    return (<ConfigCell file_name={file_name} key={index} start_editor={start_editor} />)
                 })}
             </div>
             <div className="ConfigAdd" >
@@ -78,10 +85,11 @@ function ConfigContainer(props: ConfigContainerProps) {
 interface EditorProps {
     file_name : string;
     title : string;
+    close_editor(): any
 }
 
 
-function Editor({file_name, title} : EditorProps) {
+function Editor({file_name, title, close_editor} : EditorProps) {
     return (
         <div className="Editor">
             <div className="EditorTitle">
@@ -105,25 +113,42 @@ function Configuration() {
 
     let [pallet_title, machine_title] = ["Pallet Configuration", "Machine Configuration"];
 
+    let file_name = "";
+
+    let start_pallet_editor = (fn : string) => {
+        file_name = fn;
+        set_editor("PALLET");
+    };
+
+    let start_machine_editor = (fn: string) => {
+        file_name = fn;
+        set_editor("MACHINE");
+    }
+
+    let close_editor = ()=>{
+        // Save the file in the editor.
+        set_editor("CONFIG");
+    };
+    
     switch (editor) {
         case "MACHINE" : {
             return (
-                <Editor title={pallet_title} file_name={"temp"}/>
+                <Editor title={pallet_title} file_name={"temp"} close_editor={close_editor}/>
             );
         };
         case "PALLET": {
             return (
-                <Editor title={machine_title} file_name={"temp"}/>
+                <Editor title={machine_title} file_name={"temp"} close_editor={close_editor}/>
             );
         };
-        default : {
+        default : { // ie. case : "CONFIG"
             return (
                 <div className="ConfigGrid">
                     <div className="PalletConfig">
-                        <ConfigContainer title={pallet_title} configs={pallet_configs} />
+                    <ConfigContainer title={pallet_title} configs={pallet_configs} start_editor={start_pallet_editor} />
                     </div>
                     <div className="MachineConfig">
-                        <ConfigContainer title={machine_title} configs={machine_configs} />
+                    <ConfigContainer title={machine_title} configs={machine_configs} start_editor={start_machine_editor} />
                     </div>
                 </div>
             );
