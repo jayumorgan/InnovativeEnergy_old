4378b44cbf23b974f1a755691a5dd66f6884a08e
69 <bs@email.com>
2020-07-26 19:05:16 -0400

    Pick origin location display;

--- palletizer/server/client/src/parts/TeachPallet.tsx
+++ palletizer/server/client/src/parts/TeachPallet.tsx
@@ -195,25 +195,27 @@ function JoggerDisplay() {
                     </div>
                 </div>
             </div>
-            <div className="JoggerContainerInner">
-                <A dagger={true} />
-                <div className="JoggerCircleContainer">
-                    <div className="JoggerCircle">
-                        <div className="SelectPointButton">
-                            <div className="SelectButton">
-                                <span>
-                                    SELECT
+            <div className="JoggerCentering">
+                <div className="JoggerContainerInner">
+                    <A dagger={true} />
+                    <div className="JoggerCircleContainer">
+                        <div className="JoggerCircle">
+                            <div className="SelectPointButton">
+                                <div className="SelectButton">
+                                    <span>
+                                        SELECT
 				</span>
+                                </div>
                             </div>
+                            {directions.map((d: Directions, index: number) => {
+                                return (
+                                    <ArrowImage direction={d} key={index} />
+                                )
+                            })}
                         </div>
-                        {directions.map((d: Directions, index: number) => {
-                            return (
-                                <ArrowImage direction={d} key={index} />
-                            )
-                        })}
                     </div>
+                    <A dagger={false} />
                 </div>
-                <A dagger={false} />
             </div>
         </div>
     );
@@ -242,6 +244,48 @@ function CoordinateItem({ axis, value }: CoordinateItemProps) {
     );
 }
 
+interface CrossHairProps {
+    scale: number;
+    x: number; // Percent
+    y: number; // Percent
+};
+
+function CrossHairs({ scale, x, y }: CrossHairProps) {
+    let size = scale * 10;
+
+    let x_c = `${x}%`;
+    let y_c = `${y}%`;
+
+    let fif = "50%";
+    let sev = "75%";
+    let twe = "25%";
+
+    let line_1 = {
+        x1: fif,
+        y1: sev,
+        x2: fif,
+        y2: twe
+    } as any;
+
+    let line_2 = {
+        x1: twe,
+        x2: sev,
+        y1: fif,
+        y2: fif
+    } as any;
+
+    return (
+        <svg height={size * 4} width={size * 4} x={x_c} y={y_c}>
+            <g transform="scale(1,1)">
+                <circle cx={fif} cy={fif} r={"15%"} />
+                <line {...line_1} />
+                <line {...line_2} />
+            </g>
+        </svg>
+    );
+};
+
+
 function PickLocationMap() {
 
     let coordinates = {
@@ -249,6 +293,15 @@ function PickLocationMap() {
         y: 10,
         z: 400
     } as { [key: string]: number };
+
+    // Normalize the coordinates;
+
+    let crossProps = {
+        scale: 2,
+        x: 50,
+        y: 50
+    } as CrossHairProps;
+
     return (
         <div className="PickLocationMap">
             <div className="CoordinateDisplay">
@@ -257,6 +310,14 @@ function PickLocationMap() {
                         <CoordinateItem axis={key} value={coordinates[key]} key={index} />
                     )
                 })}
+            </div>
+            <div className="Map">
+                <svg width="500" height="500">
+                    <g transform="scale(1,1)">
+                        <rect width="100%" height="100%" />
+                        <CrossHairs {...crossProps} />
+                    </g>
+                </svg>
             </div>
         </div>
     );
@@ -285,7 +346,8 @@ interface CurrentStepBarProps {
 
 function CurrentStepBar({ completion_fraction }: CurrentStepBarProps) {
     let style = {
-        width: `${completion_fraction * 100}%`
+        width: `${completion_fraction * 100
+            }% `
     } as React.CSSProperties;
     return (
         <div className="CurrentStepBar">
