ef2138ca0a9793a2570c7baf7c436f2e1e30ad20
Alex <40923759+rednexela1941@users.noreply.github.com>
2020-09-13 01:39:49 -0400

    Remove old pallet render

--- palletizer/client/src/parts/machine_config/Detection.tsx
+++ palletizer/client/src/parts/machine_config/Detection.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ContentItem, { ButtonProps } from "../teach/ContentItem";
 import { IOState } from "./IO";
-import { ControlProps } from "../MachineConfig";
+import { ControlProps } from "../shared/shared";
+
 
 // Could extend control props.
-
 interface DetectionProps extends ControlProps {
 
 };
+
+
 
 export default function Detection({ handleNext, handleBack }: DetectionProps) {
 
