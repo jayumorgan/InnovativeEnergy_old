b5b94bb00bffdb3b54f3bd841319ea84b27633bc
Max Windisch <max.windisch@vention.cc>
2020-09-25 14:04:10 -0400

    tigther z_top in standard mode (basic/safe path optimization)

--- palletizer/server/optimizer/standard.ts
+++ palletizer/server/optimizer/standard.ts
@@ -13,13 +13,13 @@ export function addActionToCoordinate(coord: Coordinate, action: ActionTypes): A
     return { ...coord, action };
 };
 
-function generatePathForBox(box: BoxCoordinate): BoxPath {
+function generatePathForBox(box: BoxCoordinate, z_top: number): BoxPath {
     let path: BoxPath = [];
 
     // Picking.
-    path.push(addActionToCoordinate(raiseOverCoordinate(box.pickLocation), ActionTypes.NONE));
+    path.push(addActionToCoordinate(raiseOverCoordinate(box.pickLocation, z_top), ActionTypes.NONE));
     path.push(addActionToCoordinate(box.pickLocation, ActionTypes.PICK));
-    path.push(addActionToCoordinate(raiseOverCoordinate(box.pickLocation), ActionTypes.NONE));
+    path.push(addActionToCoordinate(raiseOverCoordinate(box.pickLocation, z_top), ActionTypes.NONE));
 
     // Dropping.
     // The lateral approach allows to pack boxes tighter.
@@ -28,13 +28,13 @@ function generatePathForBox(box: BoxCoordinate): BoxPath {
     lateralApproach.x += 40;
     lateralApproach.y += 40;
     lateralApproach.z -= 50;
-    path.push(addActionToCoordinate(raiseOverCoordinate(lateralApproach), ActionTypes.NONE));
+    path.push(addActionToCoordinate(raiseOverCoordinate(lateralApproach, z_top), ActionTypes.NONE));
     path.push(addActionToCoordinate(lateralApproach, ActionTypes.NONE));
     lateralApproach.x -= 40;
     lateralApproach.y -= 40;
     path.push(addActionToCoordinate(lateralApproach, ActionTypes.NONE));
     path.push(addActionToCoordinate(box.dropLocation, ActionTypes.DROP));
-    path.push(addActionToCoordinate(raiseOverCoordinate(box.dropLocation), ActionTypes.NONE));
+    path.push(addActionToCoordinate(raiseOverCoordinate(box.dropLocation, z_top), ActionTypes.NONE));
 
     /*
     path.push(addActionToCoordinate(raiseOverCoordinate(box.dropLocation), ActionTypes.NONE));
@@ -45,6 +45,16 @@ function generatePathForBox(box: BoxCoordinate): BoxPath {
     return path;
 };
 
+function calculateTotalHeight(boxes: BoxCoordinate[]): number {
+    let height: number = 0, iStack: number = 0;
+    boxes.forEach((box) => {
+        if  (iStack != box.palletIndex) { return; }
+        height += box.dimensions.height;
+        iStack++;
+    });
+    return height;
+}
+
 export function generateStandardPath(pallet_config: SavedPalletConfiguration): BoxPath[] {
     const { boxCoordinates } = pallet_config;
     const { pallets } = pallet_config.config;
@@ -54,25 +64,36 @@ export function generateStandardPath(pallet_config: SavedPalletConfiguration): B
     });
 
     let paths: BoxPath[] = [];
+    let current_height: number = 0;
+    let total_height: number = calculateTotalHeight(boxCoordinates);
+    console.log("generateStandardPath: total_height=" + total_height);
 
     while (true) {
         let has_coordinates: boolean = false;
+        let bc_filtered;
         // this is by layer, but could separate by pallet.
-        boxCoordinates.filter((coord: BoxCoordinate) => {
+        (bc_filtered = boxCoordinates.filter((coord: BoxCoordinate) => {
             return (coord.stackIndex === stack_indices[coord.palletIndex]);
-        }).map((coord: BoxCoordinate) => {
+        })).map((coord: BoxCoordinate) => {
             return coord;
         }).sort((a: BoxCoordinate, b: BoxCoordinate) => {
             return b.linearPathDistance - a.linearPathDistance;
         }).forEach((b: BoxCoordinate) => {
             has_coordinates = true;
-            paths.push(generatePathForBox(b));
+            // Calculate a z_top that minimized travel time conservatively.
+            // TODO: review for multiple pallets...
+            let z_top: number = Math.max(0, Math.min(
+                b.pickLocation.z - b.dimensions.height, // never travel lower than the pick location.
+                pallet_config.config.pallets[0].corner1.z - current_height - 2 * b.dimensions.height) // travel as low as the current height minus 2 boxes.
+                - 50 /* 5cm extra safety */);
+            paths.push(generatePathForBox(b, z_top));
         });
 
         if (!has_coordinates) {
             break;
         }
 
+        current_height += bc_filtered[0].dimensions.height;
         stack_indices = stack_indices.map((v: number) => {
             return v + 1;
         });
