c5db94765e652ee1edfe1ac56d16f76b6cf32137
Alex <40923759+rednexela1941@users.noreply.github.com>
2020-10-01 09:23:57 -0400

    Temporary select button in jogger

--- palletizer/client/src/parts/teach/Jogger.tsx
+++ palletizer/client/src/parts/teach/Jogger.tsx
@@ -429,8 +429,16 @@ export default function Jogger({ selectAction, updateName, name, machineConfigId
                     <PerspectiveJogger {...perspectiveJoggerProps} />
                 </div>
             </div>
+
             <div className="Parameters">
                 <JoggerParameter {...distanceParams} />
+                <div className="Select">
+                    <div className="SelectButton" onClick={handleSelect}>
+                        <span>
+                            {"Select Point"}
+                        </span>
+                    </div>
+                </div>
                 <JoggerParameter {...speedParams} />
             </div>
         </div>
